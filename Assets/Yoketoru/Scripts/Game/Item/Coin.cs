d342e28c7023d6c7ea17f06ed8bdc349d028b589
Yu Tanaka <YRK00337@nifty.com>
2025-01-12 16:19:54 +0900

    Update Coin.cs

--- Assets/Yoketoru/Scripts/Game/Item/Coin.cs
+++ Assets/Yoketoru/Scripts/Game/Item/Coin.cs
@@ -4,15 +4,31 @@ using UnityEngine.Events;
 /// <summary>
 /// コインを拾う機能を提供
 /// </summary>
-public class Coin : MonoBehaviour, IGetCoinEmitter
+public class Coin : MonoBehaviour, IGetCoinEmitter, IStartStop
 {
     [Tooltip("得点"), SerializeField]
     int point = 100;
 
+    bool isStarted = false;
+
     public UnityEvent<int> CoinGot { get; } = new();
+
+    public void OnGameStarted()
+    {
+        isStarted = true;
+    }
+
+    public void OnGameStopped()
+    {
+        isStarted = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{point}点");
+        if (!other.CompareTag("Player")) { return; }
+        if (!isStarted) { return; }
+
+        CoinGot.Invoke(point);
+        Destroy(gameObject);
     }
 }
